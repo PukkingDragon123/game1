b6b804ced0c3ff1f1924dd3c3f2d875c65a91878
Pukkingdragon123 <beyondcode@Beyonds-MacBook-Air-29.local>
2026-02-21 18:01:42 +0700

    work

--- KEEN/Assets/PlayerMovement.cs
+++ KEEN/Assets/PlayerMovement.cs
@@ -7,6 +7,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     public float force = 200;
     private GameObject focalpoint;
+    public bool hasPower = false;
+    public float pushPower = 50;
+    [Range(2, 10)]
+    public float powerDuration = 10;
+    public float flickerDuration = 5;
+    public GameObject powerUpIndicator;
+    private float initialPowerTime = 0f;
+    public float outTime = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +24,53 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+   
+    private void FixedUpdate()
     {
         float inputy = Input.GetAxis("Vertical");
         rb.AddForce(focalpoint.transform.forward * (inputy * force * Time.deltaTime));
+        powerUpIndicator.transform.position = transform.position;
+        if (hasPower == true &&((powerDuration - (Time.time - initialPowerTime)) < outTime))
+        {
+            Debug.Log("Running out!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.CompareTag("Power"))
+        {
+            hasPower = true;
+            Destroy(other.gameObject);
+            StartCoroutine(Cooldown());
+            powerUpIndicator.SetActive(true);
+            initialPowerTime = Time.time;
+
+            
+            
+            
+        }
+    
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.CompareTag("Enemy") && hasPower == true)
+        {
+            Rigidbody enemyRb = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 pos = transform.position;
+            Vector3 enemypos = other.transform.position;
+            Vector3 dir = enemypos - pos ;
+            enemyRb.AddForce(dir.normalized * pushPower, ForceMode.Impulse);
+            
+        }
+    }
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(powerDuration);
+        hasPower = false;
+        powerUpIndicator.SetActive(false);
+
 
     }
 }
